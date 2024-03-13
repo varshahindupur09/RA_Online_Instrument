8ff436ea8f1a0a1e1592a9a00cd855bc43ade2a9
Varsha Hindupur <hindupur.v@northeastern.edu>
2024-03-13 00:26:53 -0400

    frontend CSS improved

--- front-end/src/components/NavbarElements.jsx
+++ front-end/src/components/NavbarElements.jsx
@@ -3,7 +3,7 @@ import { NavLink as Link } from "react-router-dom";
 import styled from "styled-components";
 
 export const Nav = styled.nav`
-  background: #ccc;
+  background: #fff;
   width: 200px;
   position: fixed;
   top: 100px; /* Adjusted top position to prevent overlap with the website title */
